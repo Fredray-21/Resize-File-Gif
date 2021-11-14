46d78d172f8833b8cae44d124ad4eee4ec707749
Frédéric Dabadie <dabadie.frederic@gmail.com>
2021-11-14 21:04:17 +0100

    add browse file

--- resize_gif.py
+++ resize_gif.py
@@ -1,13 +1,8 @@
+#by Fredray-21
 from PIL import Image, ImageSequence
-
-# Output (max) size
-size = 150, 150
-
-# Open source
-im = Image.open("in.gif")
-
-# Get sequence iterator
-frames = ImageSequence.Iterator(im)
+import os, sys
+import glob
+from pathlib import Path
 
 # Wrap on-the-fly thumbnail generator
 def thumbnails(frames):
@@ -16,9 +11,30 @@ def thumbnails(frames):
         thumbnail.thumbnail(size, Image.ANTIALIAS)
         yield thumbnail
 
-frames = thumbnails(frames)
+        
+# Output (max) size
+x = int(input("Size: "))
 
-# Save output
-om = next(frames) # Handle first frame separately
-#om.info = im.info # Copy sequence info
-om.save("out.gif", save_all=True, append_images=list(frames),disposal=2)
+size = x,x
+# Open a file
+
+name_path = input("Name File: ")
+path = "./"+name_path+"/"
+
+name_file = os.path.exists("./"+name_path+"_resize"+str(x)+"x"+str(x))
+if (name_file == False):
+    os.makedirs(name_path+"_resize"+str(x)+"x"+str(x))
+
+dirs = os.listdir(path)
+for file in dirs:
+    if(file.split(".")[1] =="gif"):
+        name = file.split(".")[0]
+        print(file)
+        # Open source
+        im = Image.open(path+file)
+        # Get sequence iterator
+        frames = ImageSequence.Iterator(im)
+        frames = thumbnails(frames)
+        # Save output
+        om = next(frames) # Handle first frame separately
+        om.save("./"+name_path+"_resize"+str(x)+"x"+str(x)+"/"+name+"_resize.gif", save_all=True, append_images=list(frames),disposal=2)
